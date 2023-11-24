5c226d9c09fa050e65c73c70b45e93794436d293
fengzili <fengzili@pred.vip>
2023-11-24 09:42:43 +0800

    fix bugs.

--- start.py
+++ start.py
@@ -7,4 +7,15 @@
 from utils.app import startKakaAnimation
 
 if __name__ == '__main__':
+    '''for i in range(50000):
+        import os
+        if os.path.exist("stop"):
+            print("existing stop file")
+            time.sleep(0.1)
+            continue
+        with open("stop","w") as fl:
+            fl.write("stop")
+            print("write success.")
+        import time
+        time.sleep(0.2)'''
     startKakaAnimation()
